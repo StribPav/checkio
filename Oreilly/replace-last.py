0ef907342a27b0277fceb9909244fbcb6eff08f1
Pavlo Bondar <123rpv@gmail.com>
2023-10-01 15:34:21 -0400

    Replace lat add anoter examples

--- Oreilly/replace-last.py
+++ Oreilly/replace-last.py
@@ -20,3 +20,24 @@ assert list(replace_last([1])) == [1]
 assert list(replace_last([])) == []
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
+
+
+def replace_last(line: list) -> Iterable:
+    # your code here
+    if len(line) <= 1:
+        yield from line
+    else:
+        yield line[-1]
+        yield from line[:-1]
+
+
+def replace_last(line: list) -> Iterable:
+    if line:
+        line.insert(0, line[-1])
+        line.pop(-1)
+    return line
+
+def replace_last(line: list) -> Iterable:
+    if len(line) > 0:
+        line = [line[-1]] + line[0:-1]
+    return line
